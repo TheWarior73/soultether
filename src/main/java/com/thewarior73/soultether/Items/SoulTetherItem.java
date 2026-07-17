6519b039db08a2a19245a018787d6747f432b05f
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-07-18 00:20:22 +0200

    Refactor SoulTetherItem to store link state entirely in ItemStack NBT data components and register soul chest block loot table and mineable tags

--- src/main/java/com/thewarior73/soultether/Items/SoulTetherItem.java
+++ src/main/java/com/thewarior73/soultether/Items/SoulTetherItem.java
@@ -3,39 +3,28 @@ package com.thewarior73.soultether.Items;
 import com.thewarior73.soultether.Blocks.SoulChestBlock;
 import com.thewarior73.soultether.SoulTether;
 import com.thewarior73.soultether.config.ModConfig;
-import net.minecraft.ChatFormatting;
-import net.minecraft.core.Vec3i;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.ComponentContents;
-import net.minecraft.network.chat.Style;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
 import org.jspecify.annotations.NonNull;
 
-import java.util.LinkedHashSet;
-import java.util.List;
+import java.util.Optional;
 
 public class SoulTetherItem extends Item {
     private final ModConfig.TetherTier tier;
-    private Block LinkedChest;
 
     public SoulTetherItem(Properties properties, ModConfig.TetherTier tier) {
         super(properties);
         this.tier = tier;
-        this.LinkedChest = null;
     }
 
     public ModConfig.TetherTier getTier() {
@@ -54,19 +43,34 @@ public class SoulTetherItem extends Item {
                 return InteractionResult.SUCCESS;
             }
 
-            SoulTether.LOGGER.debug("Block: {}", level.getBlockState(pos).getBlock());
-            SoulTether.LOGGER.debug("Is instance of SoulChestBlock: {}", level.getBlockState(pos).getBlock() instanceof SoulChestBlock);
-            SoulTether.LOGGER.debug("IsSameSoulChest: {}", isSameSoulChest(LinkedChest, level.getBlockState(pos).getBlock()));
+            CustomData customData = stack.get(DataComponents.CUSTOM_DATA);
+            boolean isLinkedToThisChest = false;
+            if (customData != null) {
+                CompoundTag nbt = customData.copyTag();
+                Optional<Integer> xOpt = nbt.getInt("x");
+                Optional<Integer> yOpt = nbt.getInt("y");
+                Optional<Integer> zOpt = nbt.getInt("z");
+                Optional<String> dimOpt = nbt.getString("dimension");
 
-            if (isSameSoulChest(LinkedChest, level.getBlockState(pos).getBlock())) {
+                if (xOpt.isPresent() && yOpt.isPresent() && zOpt.isPresent() && dimOpt.isPresent()) {
+                    int x = xOpt.get();
+                    int y = yOpt.get();
+                    int z = zOpt.get();
+                    String dimension = dimOpt.get();
+
+                    if (x == pos.getX() && y == pos.getY() && z == pos.getZ() && dimension.equals(level.dimension().identifier().toString())) {
+                        isLinkedToThisChest = true;
+                    }
+                }
+            }
+
+            if (isLinkedToThisChest) {
                 CustomData.update(DataComponents.CUSTOM_DATA, stack, nbt -> {
                     nbt.remove("x");
                     nbt.remove("y");
                     nbt.remove("z");
                     nbt.remove("dimension");
                 });
-
-                this.LinkedChest = null;
 
                 if (player != null) {
                     player.sendOverlayMessage(Component.translatable("item.soultether.soul_tether.tooltip.unlinked_pos", pos.getX(), pos.getY(), pos.getZ()));
@@ -79,8 +83,6 @@ public class SoulTetherItem extends Item {
                     nbt.putInt("y", pos.getY());
                     nbt.putInt("z", pos.getZ());
                     nbt.putString("dimension", level.dimension().identifier().toString());
-
-                    this.LinkedChest = level.getBlockState(pos).getBlock();
 
                     SoulTether.LOGGER.debug(nbt.toString());
                 });
@@ -95,15 +97,5 @@ public class SoulTetherItem extends Item {
         }
 
         return super.useOn(context);
-    }
-
-    private boolean isSameSoulChest(Block b1, Block b2) {
-        boolean result = false;
-        if (b1 != null && b2 != null) {
-            if (b1 instanceof SoulChestBlock && b2 instanceof SoulChestBlock) {
-                result = b1.equals(b2);
-            }
-        }
-        return result;
     }
 }
